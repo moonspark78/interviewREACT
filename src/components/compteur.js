9c067f9d9081cbdc889bd9870fb1aa8a7d454314
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-21 05:57:51 +0200

    :pushpin:

--- src/components/compteur.js
+++ src/components/compteur.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
+
+
+
+
+
+
+
+const getFullUserName = (userInfo) =>{
+  const {name: {first, last}} = userInfo;
+  return `${first} ${last}`;
+}
+
 export default function Compteur () {
     const [compteur, setCompteur] = useState(0);
     const [randomDataJSON, setRandomDataJSON] = useState("");
+    const [userInfo, setUserInfo] = useState([]);
 
     useEffect(() =>{
         fetchRandomData().then(randomData =>{
             setRandomDataJSON(randomData);
+            setUserInfo(randomData.results);
         });
     },[])
   return (
@@ -16,6 +30,15 @@ export default function Compteur () {
         <p>{compteur}</p>
         <button onClick={() => setCompteur(compteur-1)}>Incrémente</button>
         <button onClick={fetchRandomData}>Fetch les datas</button>
+        {
+          userInfo.map((userInfo,id) =>(
+            <div key={id}> 
+              <p>{getFullUserName(userInfo)}</p>
+              <img alt="" src={userInfo.picture.thumbnail}/>
+            </div>
+
+          ))
+        }
         <p>{randomDataJSON}</p>
     </div>
   )
