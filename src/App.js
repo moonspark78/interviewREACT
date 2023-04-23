9c54177539d364bee25b521cd19605b3b8200913
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-24 01:09:05 +0200

    :zap: map in array

--- src/App.js
+++ src/App.js
@@ -6,13 +6,43 @@ import { useState } from 'react';
 
 
 function App() {
+  /* on va passer le name en props vers le composant enfant child */
   const [name, setName]= useState("Souli")
+  const changeName = () =>{
+    setName("Elodie")
+  }
+  const numbers = [1,2,3,4,5,6,7,8,9,10];
+  const details =[
+    {
+      name: "Elodie",
+      age: 28,
+    },
+    {
+      name: "toto",
+      age: 2,
+    },
+    {
+      name: "tata",
+      age: 48,
+    },
+    {
+      name: "kjdfnk",
+      age: 18,
+    },
+    {
+      name: "titi",
+      age: 81,
+    },
+];
   return (
     <div className="App">
       <Compteur/>
-      <Child name={name}/>
-
-      <button onClick={() => setName("Elodie")}>Change Name</button>
+      <Child name={name} changeName={changeName}/>
+      <ul>
+        {numbers.map(num =>{
+          return <li>{num}</li>
+        })}
+      </ul>
     </div>
     
   );
--- src/components/child.js
+++ src/components/child.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export  default function Child ({name}) {
+export  default function Child ({name, changeName}) {
   return (
     <div>
         <h1>{name}</h1>
+        <button onClick={changeName}>Change  name</button>
     </div>
   )
 }
