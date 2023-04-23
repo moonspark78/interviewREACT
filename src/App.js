e05172db9f577858a6a7d24e0c3c06a8f927fc09
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-04-23 20:19:28 +0200

    :tade: passing props parent to child

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Compteur from "./components/compteur";
+import Child from "./components/child";
+import { useState } from 'react';
 
 
 function App() {
+  const [name, setName]= useState("Souli")
   return (
     <div className="App">
       <Compteur/>
+      <Child name={name}/>
     </div>
     
   );
